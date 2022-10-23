4d62abf868737c7fa5c0006069406ee96bdf85e0
MalofeevAV <9151532547@mail.ru>
2022-10-23 19:59:30 +0300

    Stage_4

--- Translator.py
+++ Translator.py
@@ -1,40 +1,39 @@
-def welcome_message(DEBUG):
-	if DEBUG:
-		language = "french"
-		word = "hello"
-	else:
-		message_1 = "Type \"en\" if you want to translate from French into English, or \"fr\" if you want to translate from English into French:\n"
-		language = input(message_1)
+def welcome_message():
+	available_languages = ('Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese', 'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Turkish')
+	print("Hello, welcome to the translator. Translator supports:")
+	[print(f"{indx}. {lang}") for indx, lang in enumerate(available_languages, 1)]
 
-		message_2 = "Type the word you want to translate:\n"
-		word = input(message_2)
+	message_1 = "Type the number of your language:\n"
+	your_language = int(input(message_1)) - 1
 
-	print(f"You chose \"{language}\" as the language to translate \"{word}\" to.")
+	message_2 = "Type the number of language you want to translate to:\n"
+	language_to_translate_to = int(input(message_2)) - 1
 
-	return language, word
+	message_3 = "Type the word you want to translate:\n"
+	word = input(message_3).lower()
+	print()
+
+	return available_languages[your_language], available_languages[language_to_translate_to], word
 
 
-def request(language, word):
-	languages = ("english-french", "french-english")[language == "en"]
-	lang = languages.split("-")[1]
-
-	url = f"https://context.reverso.net/translation/{languages}/{word}"
+def request(your_language, language_to_translate_to, word):
+	url = f"https://context.reverso.net/translation/{your_language.lower()}-{language_to_translate_to.lower()}/{word}"
 	headers = {'User-Agent': 'Mozilla/5.0'}
 
 	r = requests.get(url, headers=headers)
 
 	if r:
-		print(f"200 OK\n")
 		soup = BeautifulSoup(r.content, 'html.parser')
 
 		translations = soup.find_all("span", {"class":"display-term"})
-		print(f"{lang.capitalize()} Translations:")
-		[print(el.text.strip()) for el in translations[:5]]
+		print(f"{language_to_translate_to} Translations:")
+		[print(el.text.strip()) for el in translations]
 		print()
 
 		examples = soup.find("section", {"id":"examples-content"}).find_all("span", {"class":"text"})
-		print(f"{lang.capitalize()} Examples:")
-		[print(el.text.strip(), end="\n\n") if count%2 == 0 else print(el.text.strip()) for count, el in enumerate(examples[:10], 1)]
+		print(f"{language_to_translate_to} Examples:")
+		# [print(el.text.strip(), end=("\n\n", "\n")[count%2]) for count, el in enumerate(examples, 1)]
+		[print(el.text.strip()) for el in examples]
 
 	else:
 		print("Poor connection")
@@ -44,6 +43,4 @@ if __name__ == "__main__":
 	import requests
 	from bs4 import BeautifulSoup
 
-	DEBUG = False
-
-	request(*welcome_message(DEBUG))
+	request(*welcome_message())ы
